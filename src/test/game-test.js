77cd9b61efd5a0466424006341df56e185704c65
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-07-31 11:40:59 -0600

    Set up test files for classes with requirements and describe block

---
+++ src/test/game-test.js
@@ -0,0 +1,10 @@
+var assert = require('chai').assert;
+var Player = require('../player.js');
+var Game = require('../game.js');
+
+describe('Game', function() {
+
+
+
+
+})
---
+++ src/test/player-test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+var Player = require('../player.js');
+
+describe('Player', function() {
+
+
+
+
+})
