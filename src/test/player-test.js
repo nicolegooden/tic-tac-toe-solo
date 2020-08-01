adcf15a833c17e979fabe3ee5ee8f97745e84990
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-07-31 19:31:00 -0600

    Add unit tests to verify hasEnoughTokens defaults false and tokensOnBoard defaults zero

--- src/test/player-test.js
+++ src/test/player-test.js
@@ -1,6 +1,8 @@
 var assert = require('chai').assert;
 var Player = require('../player.js');
 
+//to test in terminal, use this command from top level directory: npm test ./src/test/player-test.js
+
 describe('Player', function() {
 
   it('should be a function', function() {
@@ -57,6 +59,14 @@ describe('Player', function() {
     //add test for saveWinsToStorage() method for local storage
     //add test for retrieveWinsFromStorage() method for local storage
 
+  it('should have zero tokens on the board by default', function() {
+    var playerOne = new Player(1, 'berner tongue', 2);
+    var playerTwo = new Player(2, 'normal berner', 1);
+
+    assert.equal(playerOne.tokensOnBoard, 0);
+    assert.equal(playerTwo.tokensOnBoard, 0);
+  });
+
   it('should track number of tokens on game board', function() {
     var playerOne = new Player(1, 'berner tongue', 2);
     var playerTwo = new Player(2, 'normal berner', 1);
@@ -65,6 +75,14 @@ describe('Player', function() {
 
     assert.equal(playerOne.tokensOnBoard, 1);
   });
+
+  it('should not have enough tokens by default', function() {
+    var playerOne = new Player(1, 'berner tongue', 2);
+    var playerTwo = new Player(2, 'normal berner', 1);
+
+    assert.equal(playerOne.hasEnoughTokens, false);
+    assert.equal(playerTwo.hasEnoughTokens, false);
+  })
 
   it('should verify if player has enough tokens on board to win', function() {
     var playerOne = new Player(1, 'berner tongue', 2);
