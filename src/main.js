447ce9b9fd376a2f58cca7c2fc4d8aac023adfef
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-02 16:42:32 -0600

    Update addToken() so that players alternate token placement

--- src/main.js
+++ src/main.js
@@ -18,16 +18,18 @@ var token2 = document.querySelector('.harper-with-smile');
 winsPlayer1.addEventListener('click', increaseRecord);
 winsPlayer2.addEventListener('click', increaseRecord);
 whoseTurn.addEventListener('click', switchTurns);
-window.addEventListener('load', showTurn); //might need to change node
 window.addEventListener('click', addToken);
 window.addEventListener('load', enlistPlayers);
 window.addEventListener('load', startGame);
+window.addEventListener('load', function showTurnWrapper() {
+  showTurn(currentPlayer1, currentPlayer2);
+});
 
 //////// event handlers ////////
 
 function enlistPlayers() {
-  var playerOne = new Player(1, 'berner tongue', 0);
-  var playerTwo = new Player(2, 'berner smile', 0);
+  var playerOne = new Player(1, 'berner tongue', 0, true);
+  var playerTwo = new Player(2, 'berner smile', 0, false);
   currentPlayer1 = playerOne;
   currentPlayer2 = playerTwo;
 };
@@ -37,25 +39,33 @@ function startGame() {
   currentGame = newGame;
 };
 
-function showTurn(player) {
-  if (currentPlayer1.hasNextTurn) {
+function addToken(event) {
+  for (var i = 0; i < allSpaces.length; i++) {
+    if (event.target === allSpaces[i] && currentPlayer1.hasCurrentTurn) {
+      currentPlayer1.placeToken(currentPlayer1, currentGame, allSpaces[i]);
+      allSpaces[i].innerHTML =
+        `<img class="harper-with-tongue" src="./assets/harper-with-tongue.jpg" alt="player-one-token">`;
+    }
+    if (event.target === allSpaces[i] && currentPlayer2.hasCurrentTurn) {
+      currentPlayer2.placeToken(currentPlayer2, currentGame, allSpaces[i]);
+      allSpaces[i].innerHTML =
+        `<img class="harper-with-smile" src="./assets/harper-with-smile.jpg" alt="player-two-token">`;
+    }
+  }
+  currentPlayer1.hasCurrentTurn = !currentPlayer1.hasCurrentTurn;
+  currentPlayer2.hasCurrentTurn = !currentPlayer2.hasCurrentTurn;
+};
+
+function showTurn(player1, player2) {
+  if (player1.hasCurrentTurn) {
     whoseTurn.innerText = `It's ${token1}'s turn!`;
   }
-  if (currentPlayer2.hasNextTurn) {
+  if (player2.hasCurrentTurn) {
     whoseTurn.innerText = `It's ${token2}'s turn!`;
   }
 };
 
-function addToken(event) {
-  for (var i = 0; i < allSpaces.length; i++) {
-    if (event.target === allSpaces[i]) {
-      allSpaces[i].innerHTML =
-        `<img class="harper-with-tongue" src="./assets/harper-with-tongue.jpg" alt="player-one-token">`;
-    }
-  }
-};
-
-function increaseRecord(player) {
+function increaseRecord() {
 
 };
 
