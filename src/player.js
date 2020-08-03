99c18ef22d7698305ce5573680b67c0a3f687c86
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-03 09:39:14 -0600

    Update placeToken() so that the space pushed to spacesTaken[] is an integer

--- src/player.js
+++ src/player.js
@@ -7,14 +7,15 @@ class Player {
   }
 
   placeToken(player, game, space) {
+    var mySpace = parseInt(space);
     game.blockedSpaces += 1;
     game.availableSpaces -= 1;
     for (var i = 0; i < game.allSpaces.length; i++) {
-      if (game.allSpaces[i] == space) {
+      if (game.allSpaces[i] == mySpace) {
         game.allSpaces.splice(i, 1);
       }
     }
-    this.spacesTaken.push(space);
+    this.spacesTaken.push(mySpace);
   }
 };
 
