d5057991403a5f52ec3a2bb21a33b571ede69a15
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-02 19:36:42 -0600

    Change winCount to default of 0 and clean up placeToken()

--- src/player.js
+++ src/player.js
@@ -1,30 +1,21 @@
 class Player {
-  constructor(id, token, winCount) {
+  constructor(id, token) {
     this.id = id;
     this.token = token;
-    this.winCount = winCount;
-    this.hasCurrentTurn = false;
+    this.winCount = 0;
     this.spacesTaken = [];
-    // this.currentSpace = 0;
-    // this.tokensOnBoard = 0;
-    // this.hasEnoughTokens = false;
   }
 
   placeToken(player, game, space) {
-    this.hasCurrentTurn = true;
-    // this.tokensOnBoard += 1;
     game.blockedSpaces += 1;
     game.availableSpaces -= 1;
     for (var i = 0; i < game.allSpaces.length; i++) {
-      if (game.allSpaces[i] === space) {
+      if (game.allSpaces[i] == space) {
         game.allSpaces.splice(i, 1);
       }
     }
     this.spacesTaken.push(space);
-    // if (this.tokensOnBoard === 3) {
-    //   this.hasEnoughTokens = true;
-    // }
   }
 };
 
-module.exports = Player;
+// module.exports = Player;
