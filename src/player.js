de7029783d8e5027b1ce4610e7929e772f415538
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-04 14:30:43 -0600

    Remove placeToken() method from player object

--- src/player.js
+++ src/player.js
@@ -7,18 +7,6 @@ class Player {
     this.hasVictory = false;
   }
 
-  placeToken(player, game, space) {
-    var mySpace = parseInt(space);
-    game.blockedSpaces += 1;
-    game.availableSpaces -= 1;
-    for (var i = 0; i < game.allSpaces.length; i++) {
-      if (game.allSpaces[i] == mySpace) {
-        game.allSpaces.splice(i, 1);
-      }
-    }
-    this.spacesTaken.push(mySpace);
-  }
-
   saveWinsToStorage() {
     if (this.id === 1) {
       var p1WinsForStorage = JSON.stringify(this.winCount);
