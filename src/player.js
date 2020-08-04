570a8520de66a2e88a51228ead2ac2dca9f66317
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-08-04 15:02:51 -0600

    Remove DOM-related code from data model

--- src/player.js
+++ src/player.js
@@ -22,12 +22,10 @@ class Player {
       var p1RetrievedWins = localStorage.getItem('player 1 wins');
       var p1ParsedWins = JSON.parse(p1RetrievedWins);
       this.winCount = p1ParsedWins;
-      winsPlayer1.innerHTML = `<h1 class="player-one-wins">${p1ParsedWins} WINS</h1>`;
     } else {
       var p2RetrievedWins = localStorage.getItem('player 2 wins');
       var p2ParsedWins = JSON.parse(p2RetrievedWins);
       this.winCount = p2ParsedWins;
-      winsPlayer2.innerHTML = `<h1 class="player-two-wins">${p2ParsedWins} WINS</h1>`;
     }
   }
 };
